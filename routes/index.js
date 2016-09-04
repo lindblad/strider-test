34629c5257a1f715095053380cf1bfa031afd7c8
Mikhail Lindblad <lindbladmisha@hotmail.com>
2016-09-04 20:35:47 +0200

    Another webhook test

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Strider test' });
+  res.render('index', { title: 'Strider test 2' });
 });
 
 module.exports = router;
